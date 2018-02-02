c7fd1cc9f5618415ad18b93491842dcc3f6c7f3d
Sandro Bonazzola <sbonazzo@redhat.com>
2018-02-02 14:26:47 +0100

    automation: switch to 4.2
    Change-Id: I8500176f76f20e677b6574156c21d3b397d3223f
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -12,8 +12,21 @@ pushd exported-artifacts
     yum reinstall -y system-release yum
     [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
-    yum install -y ovirt-release-master
+    yum install -y ovirt-release42-snapshot
     rm -f /etc/yum/yum.conf
     yum repolist enabled
-    yum --downloadonly install *$(arch).rpm
+    DISTVER="$(rpm --eval "%dist"|cut -c2-)"
+    yum clean all
+    if [[ "${DISTVER}" == "fc27" ]]; then
+        # Fedora 27 support is broken, just provide a hint on what's missing
+        # without causing the test to fail.
+        yum --downloadonly install *$(arch).rpm || true
+    elif [[ "${DISTVER}" == "fc28" ]]; then
+        # Fedora 28 support is broken, just provide a hint on what's missing
+        # without causing the test to fail.
+        yum --downloadonly install *$(arch).rpm || true
+    else
+        yum --downloadonly install *$(arch).rpm
+    fi
 popd
+
