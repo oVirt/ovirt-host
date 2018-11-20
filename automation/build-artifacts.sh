264c1d3fefe14992ecc2124e3fd15e35a990beb1
Sandro Bonazzola <sbonazzo@redhat.com>
2018-11-20 16:01:10 +0100

    build: post ovirt-host-4.3.0-0.1.beta
    Change-Id: I1cf7c2b7e8005653f7042fe5dda696e85560e8a8
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,10 +5,13 @@
 [[ -d tmp.repos/SOURCES ]] \
 || mkdir -p tmp.repos/SOURCES
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
+
 cp LICENSE tmp.repos/SOURCES
 
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ba ovirt-host.spec
 
 find \
