3a24f92af49dbffe377a7333687f04fbed90a25c
Sandro Bonazzola <sbonazzo@redhat.com>
2021-01-15 09:25:51 +0100

    automation: check also CentOS Stream
    test ovirt-host on both CentOS Linux and CentOS Stream.
    
    Change-Id: I12ad3cf8f0159b4a983bc6c9dc1f7672be8fc067
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -5,8 +5,17 @@ LC_ALL=en_US.UTF-8 rpmlint ovirt-host.spec
 ./automation/build-artifacts.sh
 
 DISTVER="$(rpm --eval "%dist"|cut -c2-4)"
-PACKAGER=""
+ARCH="$(rpm --eval "%_arch")"
 PACKAGER=dnf
+export PACKAGER
+
+on_exit() {
+    ${PACKAGER} --verbose clean all
+}
+
+trap on_exit EXIT
+
+
 
 find \
     "$PWD/tmp.repos" \
@@ -18,24 +27,44 @@ pushd exported-artifacts
     ${PACKAGER} reinstall -y system-release ${PACKAGER}
     [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
+    [[ -e /etc/dnf/dnf.conf ]] && echo "deltarpm=False" >> /etc/dnf/dnf.conf
     ${PACKAGER} install -y ovirt-release-master
     rm -f /etc/yum/yum.conf
-    if [[ "${DISTVER}" == "el" ]]; then
-        #Enable CR repo
-        sed -i "s:enabled=0:enabled=1:" /etc/yum.repos.d/CentOS-CR.repo
-    fi
     ${PACKAGER} repolist enabled
     ${PACKAGER} clean all
-    if [[ "$(rpm --eval "%_arch")" == "s390x" ]]; then
+    if [[ "${ARCH}" == "s390x" ]]; then
         # s390x support is broken, just provide a hint on what's missing
         # without causing the test to fail.
         ${PACKAGER} --downloadonly install *$(arch).rpm || true
-    elif [[ "$(rpm --eval "%_arch")" == "ppc64le" ]]; then
-        # ppc64le support is broken, just provide a hint on what's missing
-        # without causing the test to fail.
-        ${PACKAGER} --downloadonly install *$(arch).rpm || true
-    else
-        ${PACKAGER} --downloadonly install *$(arch).rpm
+    elif
+     [[ "$(rpm --eval "%dist")" == ".el8" ]]; then
+        if [[ "${ARCH}" == "ppc64le" ]]; then
+            # ppc64le support is broken, just provide a hint on what's missing
+            # without causing the test to fail.
+            ${PACKAGER} --downloadonly install *$(arch).rpm || true
+        else
+            ${PACKAGER} --downloadonly install *$(arch).rpm
+        fi
+        echo "Testing CentOS Stream"
+        ${PACKAGER} remove ovirt-release-master
+        ${PACKAGER} install -y centos-release-stream
+        ${PACKAGER} repolist enabled
+        ${PACKAGER} --releasever=8-stream --disablerepo=* --enablerepo=Stream-BaseOS download centos-stream-repos
+        ${PACKAGER} install -y centos-stream-release
+        rpm -e --nodeps centos-linux-repos
+        rpm -i centos-stream-repo*
+        rm -fv centos-stream-repo*
+        ls -l /etc/yum.repos.d/
+        ${PACKAGER} distro-sync -y
+        ${PACKAGER} install -y ovirt-release-master
+        ${PACKAGER} repolist enabled
+        ${PACKAGER} clean all
+        if [[ "$(rpm --eval "%_arch")" == "ppc64le" ]]; then
+            # ppc64le support is broken, just provide a hint on what's missing
+            # without causing the test to fail.
+            ${PACKAGER} --downloadonly install *$(arch).rpm || true
+        else
+            ${PACKAGER} --downloadonly install *$(arch).rpm
+        fi
     fi
 popd
-
