51a714be96f7eb7b64b5f90303baa8ca3f6dfede
Sandro Bonazzola <sbonazzo@redhat.com>
2021-10-01 10:31:27 +0200

    automation: switch to 4.4 repositories
    Change-Id: I25975a692a2214ff36227588c0c5f7844d24a09d
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -25,7 +25,7 @@ pushd exported-artifacts
     [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
     [[ -e /etc/dnf/dnf.conf ]] && echo "deltarpm=False" >> /etc/dnf/dnf.conf
-    ${PACKAGER} install -y ovirt-release-master
+    ${PACKAGER} install -y ovirt-release44-pre
     rm -f /etc/yum/yum.conf
     ${PACKAGER} repolist enabled
     ${PACKAGER} clean all
@@ -37,13 +37,13 @@ pushd exported-artifacts
      [[ "$(rpm --eval "%dist")" == ".el8" ]]; then
         ${PACKAGER} --downloadonly install ./*"$(arch).rpm"
         echo "Testing CentOS Stream"
-        ${PACKAGER} remove ovirt-release-master
+        ${PACKAGER} remove ovirt-release44-pre
         ${PACKAGER} install -y centos-release-stream
         ${PACKAGER} repolist enabled
         ${PACKAGER} swap centos-linux-repos centos-stream-repos
         ls -l /etc/yum.repos.d/
         ${PACKAGER} distro-sync -y
-        ${PACKAGER} install -y ovirt-release-master
+        ${PACKAGER} install -y ovirt-release44-pre
         ${PACKAGER} repolist enabled
         ${PACKAGER} clean all
         ${PACKAGER} --downloadonly install ./*"$(arch).rpm"
