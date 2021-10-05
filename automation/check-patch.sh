b91d03731ce3192d9c94684d4cc4b06626b690f1
Sandro Bonazzola <sbonazzo@redhat.com>
2021-10-05 16:23:18 +0200

    automation: check tested repo
    tested repo gets updates earlier than master snapshot.
    
    Change-Id: Ie4097dc5ce41ce19ee4f1fd129d9580248165c4e
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -30,8 +30,9 @@ pushd exported-artifacts
     [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
     [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
     [[ -e /etc/dnf/dnf.conf ]] && echo "deltarpm=False" >> /etc/dnf/dnf.conf
-    ${PACKAGER} install -y ovirt-release-master
+    ${PACKAGER} install -y ovirt-release-master-tested
     rm -f /etc/yum/yum.conf
+    ${PACKAGER} -y update
     ${PACKAGER} repolist enabled
     ${PACKAGER} clean all
     if [[ "${ARCH}" == "s390x" ]]; then
@@ -48,7 +49,8 @@ pushd exported-artifacts
         ${PACKAGER} swap centos-linux-repos centos-stream-repos
         ls -l /etc/yum.repos.d/
         ${PACKAGER} distro-sync -y
-        ${PACKAGER} install -y ovirt-release-master
+        ${PACKAGER} install -y ovirt-release-master-tested
+        ${PACKAGER} update -y
         ${PACKAGER} repolist enabled
         ${PACKAGER} clean all
         ${PACKAGER} --downloadonly install ./*"$(arch).rpm"
